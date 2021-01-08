abb6b0b078ffc976fe73fbe32bc5344f62908c07
Pawel Szopa <expiren@gmail.com>
2021-01-08 19:30:44 +0100

    base  config

---
+++ app/__init__.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask_babel import Babel, lazy_gettext as _l
+from flask_login import LoginManager
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+babel = Babel()
+login_manager = LoginManager()
+
+
+def create_app(config_env=''):  # funkcja faktory
+    app = Flask(__name__)
+
+    if not config_env:
+        config_env = app.env
+        # w app.env jest zmienna srodowiskowa zapisana, wiec  jak ustawimy FLASK_ENV to tutaj sie wpisze
+
+    app.config.from_object(f'config.{config_env.capitalize()}Config')  # liczone od source roota wiec lczy od music
+    #  jakby source root byl na app to trzeba dodać .. config
+    db.init_app(app)
+    babel.init_app(app)
+    login_manager.init_app(app)
+
+    login_manager.session_protection = 'strong'
+    login_manager.login_view = 'auth.login'
+    login_manager.login_message = _l('You need to be logged in to access this page.')
+    # default information if user want to get to page
+    # _l do multilangue _l jest lazy
+    login_manager.login_message_category = 'danger'
+
+    Migrate(app, db)
+
+    return app
---
+++ config.py
@@ -0,0 +1,28 @@
+import os
+
+base_dir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get(
+        'FLASK_SECRET_KEY') or 'top_secret'  # b to bite string - czesc baz danych nie obsluguje bite string b'321/dsa/2'
+    # os.environ.get pobiera ze zmiennej srodowiskowej secret key jesli nie mamy to jest
+    ALLOWED_IMAGE_EXTENSIONS = ['.jpg', 'jpeg', 'png']  # troche jak validator do obrazkow
+    MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # ograniczenie wielkości (kb/mb) obrazków w mb
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # wylaczanie warningow z sql alchemy
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('FLASK_DB_URI') or f'sqlite:///{os.path.join(base_dir, "music.db")}'
+    IMAGE_UPLOADS = os.environ.get('FLASK_UPLOADS_FOLDER_URI') or os.path.join(base_dir, 'uploads')
+
+
+class DevelopmentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(base_dir, "music.db")}'
+    IMAGE_UPLOADS = os.path.join(base_dir, 'uploads')
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(base_dir, "test_music.db")}'
+    IMAGE_UPLOADS = os.path.join(base_dir, 'uploads')
